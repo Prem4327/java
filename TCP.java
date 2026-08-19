b4c5acd72b5a727a129b47834689c66918c0a9c9
Prem4327 <sppremkumar0001@gmail.com>
2026-08-19 15:44:07 +0530

    Add files via upload

---
+++ TCP.java
@@ -0,0 +1,17 @@
+import java.net.*;
+
+public class TCP{
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket server = new ServerSocket(5000);
+
+        System.out.println("Waiting for client...");
+
+        Socket socket = server.accept();
+
+        System.out.println("Client connected");
+
+        socket.close();
+        server.close();
+    }
+}
